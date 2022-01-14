d248bf6d9b02e0ddf2d5f2085d05f3d77bd8ce2b
Ke Li <93945205+like101101@users.noreply.github.com>
2022-01-13 23:25:53 -0800

    Update simple_test.sh

--- test/simple_test.sh
+++ test/simple_test.sh
@@ -1,7 +1,37 @@
-if [[ ! -a kernel ]]; then
-    echo "Detected: kernel not made"
-    echo "Test: 0/1"
+#! /bin/bash
+
+#Name of File where QEMU output is stored
+test_output="test.out"
+
+#Remove old object Files
+rm unit_test.o
+rm ../unit_test.o
+
+#Make test
+make boot_test
+
+#Move Test file to UKL Directory
+cp unit_test.o ../
+
+#Navigate to UKL Directory
+cd ../
+
+#Build Test as UKL Application
+make unit_test
+
+
+#Check for Magic String to Verify that UKL Booted
+#If Test fails, print QEMU output
+if ! grep -sq "UKL Booted Successfully" "$test_output"; then
+    echo "TEST FAIL - UKL BOOT"
+    cat $test_output
+    rm -f $test_output
+    exit 1
 fi
 
-echo "Test Complete"
-echo "Hi"
+#Otherwise, print success message
+echo "TEST PASS - UKL BOOT"
+
+rm -f $test_output
+
+exit 0

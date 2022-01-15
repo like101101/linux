633b969bfda2d8b35a6237a7f5e3a0d6f3cd38a0
Ke Li <93945205+like101101@users.noreply.github.com>
2022-01-14 18:47:17 -0800

    Update simple_test.sh

--- test/simple_test.sh
+++ test/simple_test.sh
@@ -13,8 +13,11 @@ make boot_test
 #Build Test as UKL Application
 make unit_test
 
+#Run boot test
+cd ..
+cd min-initrd
+sudo timeout 10m qemu-system-x86_64 -smp cpus=1 -m 3g -no-reboot -kernel ../linux/arch/x86/boot/bzImage -initrd min-initrd.d/initrd -hda min-initrd.d/root -nodefaults -nographic -serial file:"../test.out" -append "console=ttyS0 root=/dev/sda nowatchdog nosmap nosmep mds=off"
 
-make runU -C min-initrd
 
 #Check for Magic String to Verify that UKL Booted
 #If Test fails, print QEMU output
